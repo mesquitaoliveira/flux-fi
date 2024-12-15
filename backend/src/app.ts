bcade870e6381d569c63a7142488de9c9eee2a1e
Mesquita Oliveira <ismael.mesquita.100@gmail.com>
2024-12-15 20:33:16 -0300

    feat: add payment webhook route and enhance CORS middleware for improved API security

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { qrCodeRouter } from "./routes/qrCodeRoute";
+import { paymentWebhookRouter } from "./routes/payment-webhook";
 
 const app = express();
 
@@ -13,13 +14,25 @@ const corsOptions = {
 
 // Middleware de CORS
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Habilita suporte para requisições preflight
 
+// Middleware explícito para OPTIONS
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://flux-fi.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); // Responde corretamente à requisição preflight
+});
 
+// Outros Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
 app.use("/api", qrCodeRouter);
+app.use("/api", paymentWebhookRouter);
 
 export default app;
--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -1,5 +1,9 @@
+export const ASAAS_API_URL= process.env.ASAAS_API_URL as string;
+
 export const BASE_URL = "https://sandbox.asaas.com/api/v3/pix/qrCodes/static";
 
 export const API_KEY = process.env.API_KEY_ASAAS as string;
 
-export const CHAVE_PIX = "f7c4c2f7-fc6f-4bfb-ad57-bef06c0c867b";
+export const WEBHOOK_AUTH_TOKEN = process.env.WEBHOOK_AUTH_TOKEN as string;
+
+export const CHAVE_PIX = process.env.CHAVE_PIX as string;
